5eedcd9a2a1941b6c000ee3912ae5cedb8fc6c96
Methal Abeywickrama <methal8779@gmail.com>
2021-07-09 22:53:40 +0530

    functionality added

--- script.rb
+++ script.rb
@@ -6,10 +6,14 @@ class Mastermind
     @code_breaker = breaker
     @code = code
   end
+  def check_match(input)
+    
+  end
 end
 
 class CodeMaker
   attr_reader :maker_name, :input
+  attr_writer :input
   def initialize(maker)
     @maker_name = maker
   end
@@ -19,7 +23,7 @@ class CodeMaker
 end
 
 class CodeBreaker
-  attr_reader :breaker_name
+  attr_accessor :breaker_name, :no_1, :no_2, :no_3, :no_4, :the_guess
   def initialize(breaker)
     @breaker_name = breaker
   end
@@ -28,6 +32,7 @@ class CodeBreaker
     @no_2 = guess[1]
     @no_3 = guess[2]
     @no_4 = guess[3]
+    @the_guess = guess
   end
 end
 
@@ -39,15 +44,31 @@ code_breaker = CodeBreaker.new(gets.chomp.to_s)
 puts "\n"
 
 puts "#{code_maker}, write down the code \n (4 digit number)"
-game = Mastermind.new(code_maker.maker_name, code_breaker.breaker_name, gets.chomp.to_i)
-
-if game.code == 0
-  until game.code != 0
+code_maker.declare_code
+if code_maker.input == 0 || code_maker.input.to_s.length != 4
+  until code_maker.input != 0 && code_maker.input.to_s.length == 4
     puts "The entered code was invalid \n Please input a 4 digit number"
-    game.code = gets.chomp.to_i
+    code_maker.input = gets.chomp.to_i
   end
 end
+game = Mastermind.new(code_maker.maker_name, code_breaker.breaker_name, code_maker.input.to_s)
 
-puts game.code_maker
-puts game.code_breaker
-puts game.code
+puts "#{code_breaker.breaker_name}, you have 10 attempts to crack the code."
+puts "enter your guess"
+guess = gets.chomp.to_i
+if guess == 0 || guess.to_s.length != 4
+  until guess != 0 && guess.to_s.length == 4
+    puts "The entered code was invalid \n Please input a 4 digit number"
+    guess = gets.chomp.to_i
+  end
+end
+code_breaker.guess(guess.to_s)
+
+p code_breaker.the_guess
+p game.code_maker
+p game.code_breaker
+p game.code
+p code_breaker.no_1
+p code_breaker.no_2
+p code_breaker.no_3
+p code_breaker.no_4
