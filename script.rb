0a540b434090ac7b6029fa16c8cd391e9f3955c1
Methal Abeywickrama <methal8779@gmail.com>
2021-07-13 12:29:48 +0530

    rudimentary check matches work now

--- script.rb
+++ script.rb
@@ -1,13 +1,60 @@
 class Mastermind
   attr_reader :code_maker, :code_breaker, :code
   attr_writer :code
+  attr_accessor :correct_number, :matches
   def initialize(maker,breaker,code)
     @code_maker = maker
     @code_breaker = breaker
     @code = code
+    @correct_number = 0
+    @matches = 0
   end
   def check_match(input)
-    
+    @correct_number = 0
+    @matches = 0
+    input_array = input.split(//)
+    @input_array = input.split(//)
+    @input_other_array = @input_array.clone
+    code_array = @code.split(//)
+    @code_array = @code.split(//)
+    @code_other_array = @code_array.clone
+
+    @correct_number = right_numbers(code_array, input_array)
+
+    # @correct_numbers = @code_other_array.select do |v|
+    #   # @count = 0
+    #   @input_other_array.each do |e|
+    #     if v == e
+    #       # @count += 1
+    #       @input_other_array.delete(e)
+    #       return true
+    #     end
+    #     puts @input_other_array
+    #   end
+    # end
+
+    # @correct_number = @correct_numbers.length
+
+    @code_array.each_with_index do |v, i|
+      @input_array.each_with_index do |e, x|
+        if i == x && e == v
+          @matches += 1
+        end
+
+      end
+    end
+  end
+  def right_numbers(master, guess)
+    same = 0
+    guess.each_index do |index|
+      next unless guess[index] != '*' && master.include?(guess[index])
+
+      same += 1
+      remove = master.find_index(guess[index])
+      master[remove] = '?'
+      guess[index] = '?'
+    end
+    same
   end
 end
 
@@ -53,22 +100,50 @@ if code_maker.input == 0 || code_maker.input.to_s.length != 4
 end
 game = Mastermind.new(code_maker.maker_name, code_breaker.breaker_name, code_maker.input.to_s)
 
-puts "#{code_breaker.breaker_name}, you have 10 attempts to crack the code."
-puts "enter your guess"
-guess = gets.chomp.to_i
-if guess == 0 || guess.to_s.length != 4
-  until guess != 0 && guess.to_s.length == 4
-    puts "The entered code was invalid \n Please input a 4 digit number"
-    guess = gets.chomp.to_i
-  end
-end
-code_breaker.guess(guess.to_s)
+no_of_attempts = 10
 
-p code_breaker.the_guess
-p game.code_maker
-p game.code_breaker
-p game.code
-p code_breaker.no_1
-p code_breaker.no_2
-p code_breaker.no_3
-p code_breaker.no_4
+until (game.correct_number == 4 && game.matches == 4) || no_of_attempts <= 0
+  puts "#{code_breaker.breaker_name}, you have #{no_of_attempts} attempts to crack the code."
+  puts "enter your guess"
+  guess = gets.chomp.to_i
+  if guess == 0 || guess.to_s.length != 4
+    until guess != 0 && guess.to_s.length == 4
+      puts "The entered code was invalid \n Please input a 4 digit number"
+      guess = gets.chomp.to_i
+    end
+  end
+  no_of_attempts -= 1
+  
+  # (game.correct_number != 4 && game.matches != 4) || no_of_attempts <= 0
+  #   puts "#{code_breaker.breaker_name}, you have #{no_of_attempts} attempts to crack the code."
+  #   puts "enter your guess"
+  #   guess = gets.chomp.to_i
+  #   if guess == 0 || guess.to_s.length != 4
+  #     until guess != 0 && guess.to_s.length == 4
+  #       puts "The entered code was invalid \n Please input a 4 digit number"
+  #       guess = gets.chomp.to_i
+  #     end
+  #   end
+  #   no_of_attempts -= 
+  #   # puts game.correct_number
+  #   # puts game.matches
+  # end
+  
+  code_breaker.guess(guess.to_s)
+  game.check_match(code_breaker.the_guess)
+  
+  # p code_breaker.the_guess
+  # p game.code_maker
+  # p game.code_breaker
+  # p game.code
+  # p code_breaker.no_1
+  # p code_breaker.no_2
+  # p code_breaker.no_3
+  # p code_breaker.no_4
+  # p game.correct_number
+  # p game.matches
+  
+  p no_of_attempts
+  p game.correct_number
+  p game.matches
+end
